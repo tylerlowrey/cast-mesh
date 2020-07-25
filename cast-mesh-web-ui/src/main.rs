0a97a48e40f275d599bfb6983539229b0c72a4f6
tylerlowrey <contact@tylerlowrey.com>
2020-07-25 17:07:44 -0400

    Fixed errors with ui backend

--- cast-mesh-web-ui/src/main.rs
+++ cast-mesh-web-ui/src/main.rs
@@ -1,12 +1,21 @@
-use rusqlite::Connection;
+#![feature(proc_macro_hygiene, decl_macro)]
+use rusqlite::{params, Connection, Result};
 use rocket::Config;
 use cast_mesh_ui_backend::ROCKET_SERVER_PORT;
 use rocket::config::Environment;
 use cast_mesh_ui_backend::cors::CORS;
+use cast_mesh_ui_backend::routes;
+use std::sync::{Mutex, Arc};
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 fn main() {
 
-    let conn = Connection::open(cast_mesh_server::SQLITE_DB_PATH)?;
+    let conn = Connection::open(cast_mesh_ui_backend::SQLITE_DB_PATH)
+        .expect("Unable to connect to sqlite databse");
 
     conn.execute(
         r#"CREATE TABLE IF NOT EXISTS devices(
@@ -33,6 +42,8 @@ fn main() {
         .port(ROCKET_SERVER_PORT)
         .finalize()
         .expect("Unable to configure rocket server");
+
+    let db_conn = Arc::new(Mutex::new(conn));
 
     let rocket_server = rocket::custom(config).attach(CORS());
     rocket_server
